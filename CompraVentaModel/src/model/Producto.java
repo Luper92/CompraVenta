007bdffa31d4e21c4e728ea984397661b9f8bedd
Lucio J. Quintana <quintana.unq.92@gmail.com>
2019-06-26 18:06:04 -0300

    Agregando proyectos model e interfaz...

---
+++ CompraVentaModel/src/model/Producto.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Producto {
+	public String nombre;
+	public double precio_inicial;
+	public double precio_venta;
+	
+	public Producto(String nombre, double precio_inicial, double precio_venta){
+		this.nombre = nombre;
+		this.precio_inicial = precio_inicial;
+		this.precio_venta = precio_venta;
+	}
+
+	
+}
